29cfb083e6f863ef95c9e0e61b4eb5d037a92a10
Patrick Zugel <ZugelPK20@pwcs-edu.org>
2019-05-22 13:29:26 -0400

    Flip flop.

--- haskellize.hs
+++ haskellize.hs
@@ -1,2 +1,4 @@
 tester s x = if ( odd x ) then ( reverse s ) else s
 lengther s = if ( odd ( length s ) ) then ( reverse s ) else s
+chunker [] n = []
+chunker s n = ( take n s ):( chunker ( drop n s ) n )
