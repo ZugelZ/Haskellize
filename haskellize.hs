8618bbc56727b6318d60fa8bf384fcbfa8d9da21
Patrick Zugel <ZugelPK20@pwcs-edu.org>
2019-05-22 13:43:52 -0400

    AKSJDHKAJSDH.

--- haskellize.hs
+++ haskellize.hs
@@ -2,3 +2,5 @@ tester s x = if ( odd x ) then ( reverse s ) else s
 lengther s = if ( odd ( length s ) ) then ( reverse s ) else s
 chunker [] n = []
 chunker s n = ( take n s ):( chunker ( drop n s ) n )
+
+merger = "I merged!"
